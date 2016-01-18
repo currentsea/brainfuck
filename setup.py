1cc575bfc60d0ba52ea1015dbca8a9c1bf04cd82
gfyoung <gfyoung@mit.edu>
2016-01-18 00:50:49 +0000

    Updated setup.py and MANIFEST.in
    PyPI download page was not showing the
    converted README.md file, so it was
    necessary to create an abridged version
    in RST form, which is added in this commit.
    
    Setup.py and MANIFEST.in were modified
    accordingly to include this new file.

--- setup.py
+++ setup.py
@@ -3,25 +3,8 @@ from setuptools import setup
 
 import os
 
-
-def get_descr():
-    """
-    Reads in 'README.md' and returns
-    it either in reStructuredText form
-    or in the original Markdown form.
-
-    The text stored in 'README.md'
-    will serve as the 'long_description'
-    for the 'setup' function.
-
-    """
-    try:
-        from pypandoc import convert
-        return convert("README.md", "rst", "md")
-
-    except ImportError:
-        with open("README.md", "r") as f:
-            return f.read()
+with open(os.path.join(os.path.dirname(__file__), 'PKGINFO.rst')) as pkginfo:
+    descr = pkginfo.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -33,7 +16,7 @@ setup(
     include_package_data=True,
     license="MIT License",
     description="Epic Brainfuck Interpreter",
-    long_description=get_descr(),
+    long_description=descr,
     download_url="https://github.com/gfyoung/brainfuck",
     url="https://github.com/gfyoung/brainfuck",
     author="G. Young",
